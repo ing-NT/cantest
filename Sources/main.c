7bf92e0f58613b806a27e20916f0c762da4e5832
nntt <ntstudyh@163.com>
2023-08-01 17:16:04 +0800

    修改main.c缩进

--- Sources/main.c
+++ Sources/main.c
@@ -12,34 +12,38 @@
 #define DATA_LEN_TX 8U
 static unsigned char send_data[8U] = {0x0aU, 0x01U, 0x0cU, 0x03U, 0x04U, 0x05U, 0x06U, 0x07U};
 
-void main(void) {
-  unsigned int a;
-  struct CAN_MSG msg_send,receive_msg;
-  //初始化
-  DisableInterrupts;
-  Pll_Init();
-  Rti_Init();
-  Ect_Init();
-  DDRC_DDRC4 = 1U;   //注意顺序不能和Can0_Init()调换
+void main(void)
+{
+	unsigned int a;
+  	struct CAN_MSG msg_send,receive_msg;
+ 	//初始化
+  	DisableInterrupts;
+  	Pll_Init();
+  	Rti_Init();
+  	Ect_Init();
+  	DDRC_DDRC4 = 1U;   //注意顺序不能和Can0_Init()调换
 	PORTC_PC4 = 1U;
-  Can0_Init();
+  	Can0_Init();
 	//EnableInterrupts;
 
 	//填写发送报文内容
-  msg_send.id = ID;
-  for (a = 0U; a < DATA_LEN_TX; a++) {
-    msg_send.data[a] = send_data[a];
-  }
-  msg_send.len = DATA_LEN_TX;
+  	msg_send.id = ID;
+  	for (a = 0U; a < DATA_LEN_TX; a++)
+  	{
+  		msg_send.data[a] = send_data[a];
+  	}
+  	msg_send.len = DATA_LEN_TX;
 	msg_send.RTR = 0U;
 	msg_send.prty = 0U;
 
-	for ( ; ; ) {
-	  if(Can_Receive(&receive_msg)) {
-	    Delay(4U);
-	    Can_Send(msg_send);
-	    Delay(8U);       //2s
-	  }
+	for ( ; ; )
+	{
+		if(Can_Receive(&receive_msg))
+		{
+			Delay(4U);
+	    	Can_Send(msg_send);
+	    	Delay(8U);       //2s
+	    }
 	}
 }
 /*

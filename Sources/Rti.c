a1b5c2d0757c592b2b633ee9900821c36f7dc0dc
nntt <ntstudyh@163.com>
2023-08-01 17:07:52 +0800

    修改缩进

--- Sources/Rti.c
+++ Sources/Rti.c
@@ -3,8 +3,9 @@
 #include "hidef.h"
 #include "derivative.h"
 
-void Rti_Init(void) {
-  CRGINT_RTIE = 1U;
-  //RTICTL计算：T=(RTR[3:0]+1)*2^(RTR[6:4]+9)/osc clock 与PLL开关无关
-  RTICTL = 0xb3U;   //0.01s
+void Rti_Init(void)
+{
+    CRGINT_RTIE = 1U;
+    //RTICTL计算：T=(RTR[3:0]+1)*2^(RTR[6:4]+9)/osc clock 与PLL开关无关
+    RTICTL = 0xb3U;   //0.01s
 }

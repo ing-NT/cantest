7cf8563f063d776b395cc6a3e6347fceaa5c7a87
nntt <ntstudyh@163.com>
2023-08-04 13:50:06 +0800

    修改了InterruptReceive.h中接收函数声明

--- Sources/InterruptReceive.h
+++ Sources/InterruptReceive.h
@@ -1,6 +1,6 @@
 #ifndef SOURCES_INTERRUPT_RECEIVE_H_
 #define SOURCES_INTERRUPT_RECEIVE_H_
 
-extern Bool Receive_Interrupt_Result_Flag(void);
+extern Bool Receive_InterruptResultFlag(void);
 
 #endif

c0b000541fd94155f81d739d2317ab6d92670789
nntt <ntstudyh@163.com>
2023-08-11 13:43:11 +0800

    统一修改16进制写法0x0A替换0x0a

--- Sources/Rti.c
+++ Sources/Rti.c
@@ -7,5 +7,5 @@ void Rti_Init(void)
 {
     CRGINT_RTIE = 1U;
     /* RTICTL计算：T=(RTR[3:0]+1)*2^(RTR[6:4]+9)/osc clock 与PLL开关无关 */
-    RTICTL = 0xb3U;   /* 0.01s */
+    RTICTL = 0xB3U;   /* 0.01s */
 }
